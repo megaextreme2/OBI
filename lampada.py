88d65e83e1eafedee29b85cf4ee48c6e9e06b201
megaextreme2 <56440055+megaextreme2@users.noreply.github.com>
2024-06-06 09:13:59 -0300

    Update lampada.py

--- lampada.py
+++ lampada.py
@@ -3,30 +3,18 @@ Se apertar interruptor 1(I1), lampada A desliga ou liga
 caso apertar o interruptor 2(I2), ambas as lampadas A e B
 Trocam de estado (liga ou desliga)
 """
-#Definição das variáveis
 N = int(input())
-A=0
-B=0
-if 2<=N<=100000:
-    for num in range(N):
-        apertos = int(input())
-        if apertos == 1 and A == 0:
-            A = 1 
-        elif apertos == 1 and A == 1:
-            A = 0
-        elif apertos == 2 and A == 0 and B == 0:
-            A = 1
-            B = 1
-        elif apertos == 2 and A == 1 and B == 1:
-            A = 0
-            B = 0
-        elif apertos == 2 and A == 0 and B == 1:
-            A = 1
-            B = 0
-        elif apertos == 2 and A == 1 and B == 0:
-            A = 0
-            B = 1
-        else:
-            A = 0
-            B = 0
-print(A,B)
+sequencia = list(map(int, input().split()))
+
+luz_A = 0
+luz_B = 0
+
+for i in sequencia:
+    if i == 1:
+        luz_A = 1 - luz_A
+    elif i == 2:
+        luz_A = 1 - luz_A
+        luz_B = 1 - luz_B
+
+print(luz_A)
+print(luz_B)
